cd1e765e64b6d4e5e42c442a2182d6120a7ea43a
czf <122385930@qq.com>
2020-09-10 19:27:49 +0800

    调整首页内容管理样式

--- src/components/Table/TableColumns.vue
+++ src/components/Table/TableColumns.vue
@@ -1,13 +1,19 @@
 <template>
   <div is="template">
     <el-table-column
-      v-for="({ fixed, field, label, width, children }) in columns"
+      v-for="({ fixed, field, label, validation, width, children }) in columns"
       :fixed="fixed"
       :prop="field"
       :label="label"
       :width="width"
       :key="field"
     >
+      <template slot-scope="scope">
+        <div v-if="validation && validation.type == 'image'">
+          <img :src="scope.row[field]" height="48" />
+        </div>
+        <span v-else>{{ scope.row[field] }}</span>
+      </template>
       <TableColumns v-if="children && children.length > 0" :columns="children" />
     </el-table-column>
   </div>
