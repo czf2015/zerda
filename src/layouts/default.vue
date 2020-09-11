d2e15270210648ee497f0f960c190d991139b383
czf <122385930@qq.com>
2020-09-12 07:23:53 +0800

    提炼home组件为通用layout

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -2,7 +2,16 @@
   <div class="page" v-show="!loading">
     <AppHeader />
     <main :style="{margin}">
-      <slot />
+      <draggable
+        v-bind="dragOptions"
+        tag="div"
+        class="item-container"
+        :list="list"
+        :value="value"
+        @input="emitter"
+      >
+        <component v-for="(data) in currentValue" :key="data.id" :is="data.type" :initial="data" />
+      </draggable>
     </main>
     <AppAside />
     <AppFooter />
@@ -14,22 +23,59 @@
 import AppHeader from "./partials/AppHeader";
 import AppFooter from "./partials/AppFooter";
 import AppAside from "./partials/AppAside";
+import Container from "@/components/Container";
+import TableFormPanel from "@/components/Panel/TableForm";
+import draggable from "vuedraggable";
 
 export default {
   components: {
     AppHeader,
     AppFooter,
     AppAside,
+    draggable,
+    Container,
+    TableFormPanel,
   },
 
   props: {
     margin: {
       type: String,
     },
-    loading: {
-      type: Boolean,
-      default: false,
+  },
+  data() {
+    return {
+      loading: true,
+      list: [],
+      dragOptions: {
+        animation: 0,
+        group: "description",
+        disabled: false,
+        ghostClass: "ghost",
+      },
+      value: null,
+    };
+  },
+  computed: {
+    currentValue() {
+      return this.value ? this.value : this.list;
     },
+  },
+  methods: {
+    emitter(value) {
+      this.$emit("input", value);
+    },
+    fetchData(page) {
+      this.loading = true;
+      fetch(`/data/${page}/index.json`)
+        .then((res) => res.json())
+        .then((data) => {
+          this.list = data;
+          this.loading = false;
+        });
+    },
+  },
+  mounted() {
+    this.fetchData(this.$route.name);
   },
 };
 </script>
--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -1,84 +1,15 @@
 <template>
-  <Layout :show="show">
-    <draggable
-      v-bind="dragOptions"
-      tag="div"
-      class="item-container"
-      :list="list"
-      :value="value"
-      @input="emitter"
-    >
-      <!-- <div v-for="(data) in currentValue" :key="data.id">
-        <component v-if="data.type !== 'Container'" :is="data.type" :initial="data" />
-        <div v-else>{{data.type}}</div>
-      </div>-->
-      <component v-for="(data) in currentValue" :key="data.id" :is="data.type" :initial="data" />
-    </draggable>
-  </Layout>
+  <Layout />
 </template>
 
 
 <script>
 import Layout from "@/layouts/default";
-import Container from "@/components/Container";
-import TableFormPanel from "@/components/Panel/TableForm";
-import draggable from "vuedraggable";
 
 export default {
   name: "HomePage",
   components: {
     Layout,
-    draggable,
-    Container,
-    TableFormPanel,
-  },
-  data() {
-    return {
-      show: false,
-      list: [],
-      dragOptions: {
-        animation: 0,
-        group: "description",
-        disabled: false,
-        ghostClass: "ghost",
-      },
-      value: null,
-    };
-  },
-  computed: {
-    currentValue() {
-      return this.value ? this.value : this.list;
-    },
-  },
-  methods: {
-    emitter(value) {
-      // console.log(value);
-      this.$emit("input", value);
-    },
-  },
-  mounted() {
-    fetch("/data/home/index.json")
-      .then((res) => res.json())
-      .then((data) => {
-        this.list = data;
-        this.show = true;
-      });
   },
 };
 </script>
-
-
-<style scoped>
-.item-container {
-  max-width: 20rem;
-  margin: 0;
-}
-.item {
-  padding: 1rem;
-  border: solid black 1px;
-  background-color: #fefefe;
-}
-.item-sub {
-  margin: 0 0 0 1rem;
-}
-</style>
